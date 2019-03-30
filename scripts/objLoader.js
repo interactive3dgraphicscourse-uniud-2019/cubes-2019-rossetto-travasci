ff91e06c437223746e7eb1ea81601628f5ca0098
Erhtric <38790094+Erhtric@users.noreply.github.com>
2019-03-31 00:01:29 +0100

    Update objLoader.js

--- scripts/objLoader.js
+++ scripts/objLoader.js
@@ -1,7 +1,7 @@
 /*
     That method loads an object and it adds to the list of avaiable boxes
     @param url : the url of the object to loaded
-    @param freeBoces : a list of avaiable boxes to be used
+    @param freeBoxes : a list of avaiable boxes to be used
 */
 function objLoader(url, freeBoxes) {
     var loader = new THREE.ObjectLoader();
