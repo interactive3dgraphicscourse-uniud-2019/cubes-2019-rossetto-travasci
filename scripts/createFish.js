3ca656c264e9843c8d67c1df255838f620f3b951
Erhtric <erhtric@users.noreply.github.com>
2019-03-31 15:09:03 +0200

    Update main

--- scripts/createFish.js
+++ scripts/createFish.js
@@ -8,38 +8,51 @@
 function createBlueFish() {
 
     // Geometries and materials
-    var body_geometry = new THREE.BoxBufferGeometry( 0.23, 0.195, 0.222 );
+    var body_geometry = new THREE.BoxBufferGeometry( 0.22, 0.18, 0.18 );
     var body_material = new THREE.MeshBasicMaterial(
-        { color: 0x0060fc }
+        { color: 0x0060fc,
+          side: THREE.FrontSide }
     );
 
-    var tail1_geometry = new THREE.BoxBufferGeometry( 0.234, 0.148, 0.152 );
+    var muzzle_geometry = new THREE.BoxBufferGeometry( 0.1, 0.07, 0.07 );
+    var muzzle_material = new THREE.MeshBasicMaterial(
+        { color: 0xa9f9f9,
+          side: THREE.FrontSide }
+    );
+
+    var tail1_geometry = new THREE.BoxBufferGeometry( 0.23, 0.148, 0.14 );
     var tail1_material = new THREE.MeshBasicMaterial(
-        { color: 0x1fb8de }
+        { color: 0x1fb8de,
+          side: THREE.FrontSide }
     );
 
-    var tail2_geometry = new THREE.BoxBufferGeometry( 0.226, 0.09, 0.102 );
+    var tail2_geometry = new THREE.BoxBufferGeometry( 0.23, 0.09, 0.102 );
 
-    var backfin_geometry = new THREE.BoxBufferGeometry( 0.054, 0.02, 0.005 );
+    var backfin_geometry = new THREE.BoxBufferGeometry( 0.08, 0.02, 0.005 );
     var fin_material = new THREE.MeshBasicMaterial(
-        { color: 0x404040 }
+        { color: 0x13FF00, 
+          side: THREE.FrontSide }
     );
 
     var topfin_geometry = new THREE.BoxBufferGeometry( 0.14, 0.074, 0.010);
 
-    var eye_geometry = new THREE.BoxBufferGeometry( 0.05, 0.24, 0.038 );
+    var eye_geometry = new THREE.BoxBufferGeometry( 0.05, 0.19, 0.038 );
     var eye_material = new THREE.MeshBasicMaterial(
-        { color: 0x000000 }
+        { color: 0x000000,
+          side: THREE.FrontSide }
     );
 
     // Meshes
     var body = new THREE.Mesh(body_geometry, body_material);
+    var muzzle = new THREE.Mesh(muzzle_geometry, muzzle_material);
     var tail1 = new THREE.Mesh(tail1_geometry, tail1_material);
     var tail2 = new THREE.Mesh(tail2_geometry, body_material);
     var backfin1 = new THREE.Mesh(backfin_geometry, fin_material);
     var backfin2 = new THREE.Mesh(backfin_geometry, fin_material);
     var topfin = new THREE.Mesh(topfin_geometry, fin_material);
     var eye = new THREE.Mesh(eye_geometry, eye_material);
+
+    muzzle.position.set(-0.09, 0, 0);
     tail1.position.set(0.04, 0.0, 0);
     tail2.position.set(0.08, 0, 0);
     backfin1.position.set(0.111, 0, 0);
@@ -52,6 +65,8 @@ function createBlueFish() {
 
     body.castShadow = true;
     body.receiveShadow = true;
+    muzzle.castShadow = true;
+    muzzle.receiveShadow = true;
     tail1.castShadow = true;
     tail1.receiveShadow = true;
     tail2.castShadow = true;
@@ -64,6 +79,7 @@ function createBlueFish() {
     backfin2.receiveShadow = true;
     
     // Hierarchy
+    body.add(muzzle);
     body.add(tail1);
     body.add(tail2);
     tail2.add(backfin1);
